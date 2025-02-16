2744079a989bbb0ffedabb8d16c467086a8664d8
Douglas Barbosa <hellodev@douglasbarbosa.com>
2025-02-16 20:09:22 -0300

    feat: Problem 1018 solved

---
+++ PortalsCodingChallenges/Beecrowd/Problem1018.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using PortalsCodingChallenges.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalsCodingChallenges.Beecrowd
+{
+  public static class Problem1018
+  {
+    public static void Run(int value)
+    {
+      int[] notes = [100, 50, 20, 10, 5, 2, 1];
+      Console.WriteLine(value);
+      foreach (int note in notes)
+      {
+        int count = value / note;
+        Console.WriteLine($"{count} nota (s) de R$ {note},00");
+        value %= note;
+      }
+    }
+  }
+}
